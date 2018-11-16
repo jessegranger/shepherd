f5b4c84eab4fd182dc9f1d7722851d50301e9e3f
Jesse Granger <jesse_granger@fastmail.com>
2018-11-16 11:25:51 -0800

    update __VERSION__ in src/client/index as part of release

--- release.sh
+++ release.sh
@@ -10,9 +10,12 @@ echo Making new release: $NEW from current release: $OLD... && \
 	echo Patching package.json... && \
 	sed -i.bak -e "s/\"version\": \"$OLD\"/\"version\": \"$NEW\"/" package.json && \
 	rm package.json.bak && \
+	echo Patching __VERSION__ in src/client/index.coffee... && \
+	sed -i.bak -e "s/__VERSION__ = '$OLD'/__VERSION__ = '$NEW'/" src/client/index.coffee && \
+	rm src/client/index.coffee.bak && \
 	echo Writing VERSION file... && \
 	echo $NEW > VERSION &&
 	echo Committing package.json && \
-	git commit --no-gpg-sign package.json VERSION -m "v$NEW" &> /dev/null && \
+	git commit --no-gpg-sign package.json VERSION src/client/index.coffee -m "v$NEW" &> /dev/null && \
 	echo Publishing to npm... && \
 	npm publish
